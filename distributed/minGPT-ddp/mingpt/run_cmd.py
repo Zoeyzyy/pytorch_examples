825563cf6c4518b65329f1c85d5d2e8b20cd2be6
Zoeyzyy <21110240040@m.fudan.edu.cn>
2024-03-18 21:00:06 +0800

    Fix: only packet header

--- distributed/minGPT-ddp/mingpt/run_cmd.py
+++ distributed/minGPT-ddp/mingpt/run_cmd.py
@@ -7,8 +7,8 @@ with open("cmd.sh", "w") as file:
     for ip in all_ip:
         # 向文件中写入内容
         if(ip != local_ip):
-            file.write("sudo tcpdump host 172.22.5." + str(ip) + " -w ../../../from" + str(ip) +".pcap & \n")
-            file.write("sudo tcpdump dst 172.22.5." + str(ip) + " -w ../../../to" + str(ip) +".pcap & \n")
+            file.write("sudo tcpdump host 172.22.5." + str(ip) + " -s 128  -w ../../../from" + str(ip) +".pcap & \n")
+            file.write("sudo tcpdump dst 172.22.5." + str(ip) + " -s 128  -w ../../../to" + str(ip) +".pcap & \n")
     
     file.write("\n")  
     file.write("torchrun --master_addr=172.22.5.106 --master_port=22349 --nproc_per_node=1 --nnodes=" + str(len(all_ip)) +" --node_rank="+ str(all_ip.index(local_ip)) +" main.py \n")  
