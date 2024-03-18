c8353b852e110e5efeaf3a42f798d0f1fdb33e3e
Zoeyzyy <1162651439@qq.com>
2024-03-18 18:40:25 +0800

    Update: remove snapshot before every training

--- distributed/minGPT-ddp/mingpt/cmd.sh
+++ distributed/minGPT-ddp/mingpt/cmd.sh
@@ -1,9 +1,10 @@
-sudo tcpdump host 172.22.5.106 > ../../../from106.pcap &
-sudo tcpdump host 172.22.5.108 > ../../../from108.pcap &
-sudo tcpdump dst 172.22.5.106 > ../../../to106.pcap &
-sudo tcpdump dst 172.22.5.108 > ../../../to108.pcap &
+rm gpt_snapshot.pt
 
+sudo tcpdump host 172.22.5.112 > ../../../from112.pcap &
+sudo tcpdump host 172.22.5.108 > ../../../from108.pcap &
+sudo tcpdump dst 172.22.5.112 > ../../../to112.pcap &
+sudo tcpdump dst 172.22.5.108 > ../../../to108.pcap &
 torchrun \
 --master_addr=172.22.5.106 --master_port=22349 \
---nproc_per_node=1 --nnodes=3 --node_rank= \
+--nproc_per_node=1 --nnodes=3 --node_rank=0 \
 main.py
