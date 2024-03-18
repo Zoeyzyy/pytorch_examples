20cdce7b00c12a2c5a25fc0b681c7af575ff270a
Zoeyzyy <21110240040@m.fudan.edu.cn>
2024-03-18 20:50:00 +0800

    Fix: tcpdump

--- distributed/minGPT-ddp/mingpt/parse.py
+++ distributed/minGPT-ddp/mingpt/parse.py
@@ -43,22 +43,34 @@ def find_max_key_less_than_x(dictionary, x):
                 max_key = key
     return max_key
 
+import re
+
+def is_valid_time(time_str):
+    pattern = r'^\d{2}:\d{2}:\d{2}\.\d+$'
+    return bool(re.match(pattern, time_str))
+
 # 遍历每一行
 for line in lines:
     # 检查是否包含[P.]，并提取时间和数字
     if '[P.]' in line and 'length' in line:
         parts = line.split()
         time_str = parts[0]  # 时间字符串
-        length_index = parts.index('length')  # 查找 'length' 的位置
-        x_index = length_index + 1  # 数字的位置在 'length' 之后
-        if x_index < len(parts):
-            x = float(parts[x_index])
-            if x != 0:
-                # 将数字累加到对应时间点
-                max_key = find_max_key_less_than_x(epoch_times, time_str)
-                if max_key is not None:
-                    epoch_time = epoch_times[max_key]
-                    time_sums[epoch_time] = time_sums.get(epoch_time, 0) + x
+        if is_valid_time(parts[0]):
+            length_index = parts.index('length')  # 查找 'length' 的位置
+            x_index = length_index + 1  # 数字的位置在 'length' 之后
+            if x_index < len(parts):
+                if not parts[x_index].isdigit():
+                    parts[x_index] = parts[x_index][:-1]
+                try:
+                    x = float(parts[x_index])
+                    if x != 0:
+                        # 将数字累加到对应时间点
+                        max_key = find_max_key_less_than_x(epoch_times, time_str)
+                        if max_key is not None:
+                            epoch_time = epoch_times[max_key]
+                            time_sums[epoch_time] = time_sums.get(epoch_time, 0) + x
+                except ValueError:
+                    print(line)
 
 # 画图
 import matplotlib.pyplot as plt
--- distributed/minGPT-ddp/mingpt/parse_by_time.py
+++ distributed/minGPT-ddp/mingpt/parse_by_time.py
@@ -46,18 +46,23 @@ def find_max_key_less_than_x(dictionary, x):
                 max_key = key
     return max_key
 
+import re
+
+def is_valid_time(time_str):
+    pattern = r'^\d{2}:\d{2}:\d{2}\.\d+$'
+    return bool(re.match(pattern, time_str))
+
 # 遍历每一行
 for line in lines:
     # 检查是否包含[P.]，并提取时间和数字
-    if '[P.]' in line and 'length' in line:
+    if 'length' in line:
         parts = line.split()
-        time_str = parts[0]  # 时间字符串
-        time_str = time_str[:accuracy]
-        length_index = [i for i, part in enumerate(parts) if 'length' in part]  # 查找 'length' 的位置
-        x_index = length_index[0] + 1  # 数字的位置在 'length' 之后
-        if x_index < len(parts) :
+        if is_valid_time(parts[0]):
+            time_str = parts[0]  # 时间字符串
+            time_str = time_str[:accuracy]
+            x_index = len(parts) - 1
             try:
-                if not parts[x_index].isdigit():
+                while(len(parts[x_index]) > 0 and not parts[x_index].isdigit()):
                     parts[x_index] = parts[x_index][:-1]
                 x = float(parts[x_index])
                 if x != 0:
@@ -65,6 +70,10 @@ for line in lines:
                     time_sums[time_str] = time_sums.get(time_str, 0) + x
             except ValueError:
                 print(line)
+                print("!")
+        else:
+            print(parts[0])
+            pass
 
 # 画图
 import matplotlib.pyplot as plt
@@ -76,7 +85,6 @@ i = 0
 for time_key, sum_value in time_sums.items():
     times.append(float(time_key[6:accuracy]))
     sums.append(sum_value)
-print(times[0])
 print(len(times))
 print(len(sums))
 
@@ -89,7 +97,8 @@ def adjust_list(lst):
     first_element = lst[0]
     return [x - first_element for x in lst]
 
-times = adjust_list(times)
+if len(times) > 0:
+    times = adjust_list(times)
 
 # 绘制柱状图
 plt.bar(times, sums, color='skyblue')
