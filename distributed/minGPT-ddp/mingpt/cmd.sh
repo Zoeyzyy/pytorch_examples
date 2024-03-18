22e8ce1ce98260a266014b048e0bd2744a931b58
Zoeyzyy <21110240040@m.fudan.edu.cn>
2024-03-18 18:33:02 +0800

    Update: cmd.sh

--- distributed/minGPT-ddp/mingpt/cmd.sh
+++ distributed/minGPT-ddp/mingpt/cmd.sh
@@ -1,4 +1,9 @@
+sudo tcpdump host 172.22.5.106 > ../../../from106.pcap &
+sudo tcpdump host 172.22.5.108 > ../../../from108.pcap &
+sudo tcpdump dst 172.22.5.106 > ../../../to106.pcap &
+sudo tcpdump dst 172.22.5.108 > ../../../to108.pcap &
+
 torchrun \
 --master_addr=172.22.5.106 --master_port=22349 \
---nproc_per_node=1 --nnodes=2 --node_rank=0 \
+--nproc_per_node=1 --nnodes=3 --node_rank= \
 main.py
