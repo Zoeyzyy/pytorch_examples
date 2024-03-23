26a129c4e63967e7090632537e017bcd9b39f76d
Zoeyzyy <1162651439@qq.com>
2024-03-23 18:43:10 +0800

    Update: multiprocess when drawing epoch time

--- distributed/minGPT-ddp/mingpt/draw_for_epoch_time0001_from.py
+++ distributed/minGPT-ddp/mingpt/draw_for_epoch_time0001_from.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from106_0_0001 = np.loadtxt('Sum_by_Time_from106_0.0001').tolist()
-from108_0_0001 = np.loadtxt('Sum_by_Time_from108_0.0001').tolist()
-from112_0_0001 = np.loadtxt('Sum_by_Time_from112_0.0001').tolist()
+from106_0_0001 = np.loadtxt('./Sum_by_time/Sum_by_Time_from106_0.0001').tolist()
+from108_0_0001 = np.loadtxt('./Sum_by_time/Sum_by_Time_from108_0.0001').tolist()
+from112_0_0001 = np.loadtxt('./Sum_by_time/Sum_by_Time_from112_0.0001').tolist()
 
-step106 = np.loadtxt('step2time_index_106').tolist()
-step108 = np.loadtxt('step2time_index_108').tolist()
-step112 = np.loadtxt('step2time_index_112').tolist()
+step106 = np.loadtxt('./step2time_index/step2time_index_106_13').tolist()
+step108 = np.loadtxt('./step2time_index/step2time_index_108_13').tolist()
+step112 = np.loadtxt('./step2time_index/step2time_index_112_13').tolist()
 
 def first_nonzero_index(lst):
     for i, num in enumerate(lst):
@@ -48,7 +48,7 @@ label = ["from 106","from 108","from 112"]
 
 colors = ['blue', 'orange', 'green', 'red']
 
-for epoch_i in range(0, 10):
+def draw(epoch_i):
     if epoch_i == 9:
         x_l = min(step106[epoch_i], step108[epoch_i], step112[epoch_i])
         x_r = position_r
@@ -56,13 +56,13 @@ for epoch_i in range(0, 10):
         x_l = min(step106[epoch_i], step108[epoch_i], step112[epoch_i])
         x_r = max(step106[epoch_i + 1], step108[epoch_i + 1], step112[epoch_i + 1])
     if epoch_i != 9:
-        data1 = ([0] * (int(step106[epoch_i]))) + (from106_0_0001[int(step106[epoch_i]): int(step106[epoch_i + 1])]) + ([0] * (int(x_r) - int(step106[epoch_i + 1])))
-        data2 = ([0] * (int(step108[epoch_i]))) + (from108_0_0001[int(step108[epoch_i]): int(step108[epoch_i + 1])]) + ([0] * (int(x_r) - int(step108[epoch_i + 1])))
-        data3 = ([0] * (int(step112[epoch_i]))) + (from112_0_0001[int(step112[epoch_i]): int(step112[epoch_i + 1])]) + ([0] * (int(x_r) - int(step112[epoch_i + 1])))
+        data1 = ([0] * (int(step106[epoch_i]) - int(x_l))) + (from106_0_0001[int(step106[epoch_i]): int(step106[epoch_i + 1])]) + ([0] * (int(x_r) - int(step106[epoch_i + 1])))
+        data2 = ([0] * (int(step108[epoch_i]) - int(x_l))) + (from108_0_0001[int(step108[epoch_i]): int(step108[epoch_i + 1])]) + ([0] * (int(x_r) - int(step108[epoch_i + 1])))
+        data3 = ([0] * (int(step112[epoch_i]) - int(x_l))) + (from112_0_0001[int(step112[epoch_i]): int(step112[epoch_i + 1])]) + ([0] * (int(x_r) - int(step112[epoch_i + 1])))
     else:
-        data1 = ([0] * (int(step106[epoch_i]) - int(x_l))) + (from106_0_0001[int(step106[epoch_i]): int(x_r)])
-        data2 = ([0] * (int(step108[epoch_i]) - int(x_l))) + (from108_0_0001[int(step108[epoch_i]): int(x_r)])
-        data3 = ([0] * (int(step112[epoch_i]) - int(x_l))) + (from112_0_0001[int(step112[epoch_i]): int(x_r)])
+        data1 = ([0] * (int(step106[epoch_i]) - int(x_l))) + (from106_0_0001[int(step106[epoch_i]): int(x_l + 200000)])
+        data2 = ([0] * (int(step108[epoch_i]) - int(x_l))) + (from108_0_0001[int(step108[epoch_i]): int(x_l + 200000)])
+        data3 = ([0] * (int(step112[epoch_i]) - int(x_l))) + (from112_0_0001[int(step112[epoch_i]): int(x_l + 200000)])
 
     print(epoch_i, len(data1), len(data2), len(data3))
     assert len(data1) == len(data2) and len(data3) == len(data2), "data 1 2 3 不长度相等"
@@ -71,7 +71,7 @@ for epoch_i in range(0, 10):
     time_interval = 0.0001
     # x 坐标范围
     x_length = len(data1)
-    x_values = [i * time_interval for i in range(len(data1))]
+    x_values = [i * time_interval * 0.1 for i in range(len(data1))]
 
     # 设置子图布局
     fig, axs = plt.subplots(3, 1, figsize=(8, 10))
@@ -79,20 +79,20 @@ for epoch_i in range(0, 10):
     # 绘制第一个子图
     axs[0].bar(range(len(data1)), data1, color = colors[0])
     axs[0].set_title('from 106')
-    axs[0].set_xlabel('Time (0.0001s)')
-    axs[0].set_ylabel('Packet Length Sum')
+    axs[0].set_xlabel('Time (ms)')
+    axs[0].set_ylabel('Packet Length Sum (Bytes)')
 
     # 绘制第二个子图
     axs[1].bar(range(len(data2)), data2, color = colors[1])
     axs[1].set_title('from 108')
-    axs[1].set_xlabel('Time (0.0001s)')
-    axs[1].set_ylabel('Packet Length Sum')
+    axs[1].set_xlabel('Time (ms)')
+    axs[1].set_ylabel('Packet Length Sum (Bytes)')
 
     # 绘制第三个子图
     axs[2].bar(range(len(data3)), data3, color = colors[2])
     axs[2].set_title('from 112')
-    axs[2].set_xlabel('Time (0.0001s)')
-    axs[2].set_ylabel('Packet Length Sum')
+    axs[2].set_xlabel('Time (ms)')
+    axs[2].set_ylabel('Packet Length Sum (Bytes)')
 
 
     # 设置整个图的标题
@@ -102,4 +102,13 @@ for epoch_i in range(0, 10):
     plt.tight_layout()
 
     # 展示图像
-    plt.savefig("from0_0001s_epoch" + str(epoch_i + 1) + ".png")
+    plt.savefig("./epoch_time/from0_0001s/from0_0001s_epoch" + str(epoch_i + 1) + ".png")
+
+import multiprocessing
+if __name__ == "__main__":
+    pool = multiprocessing.Pool(processes=10)
+    for epoch_i in range(0, 10):
+        pool.apply_async(draw, (epoch_i, ))
+    pool.close()
+    pool.join()
+    print("Sub-process(es) done.")
--- distributed/minGPT-ddp/mingpt/draw_for_epoch_time001_from.py
+++ distributed/minGPT-ddp/mingpt/draw_for_epoch_time001_from.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from106_0_001 = np.loadtxt('Sum_by_Time_from106_0.001').tolist()
-from108_0_001 = np.loadtxt('Sum_by_Time_from108_0.001').tolist()
-from112_0_001 = np.loadtxt('Sum_by_Time_from112_0.001').tolist()
+from106_0_001 = np.loadtxt('./Sum_by_time/Sum_by_Time_from106_0.001').tolist()
+from108_0_001 = np.loadtxt('./Sum_by_time/Sum_by_Time_from108_0.001').tolist()
+from112_0_001 = np.loadtxt('./Sum_by_time/Sum_by_Time_from112_0.001').tolist()
 
-step106 = np.loadtxt('step2time_index_106').tolist()
-step108 = np.loadtxt('step2time_index_108').tolist()
-step112 = np.loadtxt('step2time_index_112').tolist()
+step106 = np.loadtxt('./step2time_index/step2time_index_106_12').tolist()
+step108 = np.loadtxt('./step2time_index/step2time_index_108_12').tolist()
+step112 = np.loadtxt('./step2time_index/step2time_index_112_12').tolist()
 
 def first_nonzero_index(lst):
     for i, num in enumerate(lst):
@@ -48,7 +48,7 @@ label = ["from 106","from 108","from 112"]
 
 colors = ['blue', 'orange', 'green', 'red']
 
-for epoch_i in range(0, 10):
+def draw(epoch_i):
     if epoch_i == 9:
         x_l = min(step106[epoch_i], step108[epoch_i], step112[epoch_i])
         x_r = position_r
@@ -56,13 +56,13 @@ for epoch_i in range(0, 10):
         x_l = min(step106[epoch_i], step108[epoch_i], step112[epoch_i])
         x_r = max(step106[epoch_i + 1], step108[epoch_i + 1], step112[epoch_i + 1])
     if epoch_i != 9:
-        data1 = ([0] * (int(step106[epoch_i]))) + (from106_0_001[int(step106[epoch_i]): int(step106[epoch_i + 1])]) + ([0] * (int(x_r) - int(step106[epoch_i + 1])))
-        data2 = ([0] * (int(step108[epoch_i]))) + (from108_0_001[int(step108[epoch_i]): int(step108[epoch_i + 1])]) + ([0] * (int(x_r) - int(step108[epoch_i + 1])))
-        data3 = ([0] * (int(step112[epoch_i]))) + (from112_0_001[int(step112[epoch_i]): int(step112[epoch_i + 1])]) + ([0] * (int(x_r) - int(step112[epoch_i + 1])))
+        data1 = ([0] * (int(step106[epoch_i]) - int(x_l))) + (from106_0_001[int(step106[epoch_i]): int(step106[epoch_i + 1])]) + ([0] * (int(x_r) - int(step106[epoch_i + 1])))
+        data2 = ([0] * (int(step108[epoch_i]) - int(x_l))) + (from108_0_001[int(step108[epoch_i]): int(step108[epoch_i + 1])]) + ([0] * (int(x_r) - int(step108[epoch_i + 1])))
+        data3 = ([0] * (int(step112[epoch_i]) - int(x_l))) + (from112_0_001[int(step112[epoch_i]): int(step112[epoch_i + 1])]) + ([0] * (int(x_r) - int(step112[epoch_i + 1])))
     else:
-        data1 = ([0] * (int(step106[epoch_i]) - int(x_l))) + (from106_0_001[int(step106[epoch_i]): int(x_r)])
-        data2 = ([0] * (int(step108[epoch_i]) - int(x_l))) + (from108_0_001[int(step108[epoch_i]): int(x_r)])
-        data3 = ([0] * (int(step112[epoch_i]) - int(x_l))) + (from112_0_001[int(step112[epoch_i]): int(x_r)])
+        data1 = ([0] * (int(step106[epoch_i]) - int(x_l))) + (from106_0_001[int(step106[epoch_i]): int(x_l + 20000)])
+        data2 = ([0] * (int(step108[epoch_i]) - int(x_l))) + (from108_0_001[int(step108[epoch_i]): int(x_l + 20000)])
+        data3 = ([0] * (int(step112[epoch_i]) - int(x_l))) + (from112_0_001[int(step112[epoch_i]): int(x_l + 20000)])
 
     print(epoch_i, len(data1), len(data2), len(data3))
     assert len(data1) == len(data2) and len(data3) == len(data2), "data 1 2 3 不长度相等"
@@ -79,20 +79,20 @@ for epoch_i in range(0, 10):
     # 绘制第一个子图
     axs[0].bar(range(len(data1)), data1, color = colors[0])
     axs[0].set_title('from 106')
-    axs[0].set_xlabel('Time (0.001s)')
-    axs[0].set_ylabel('Packet Length Sum')
+    axs[0].set_xlabel('Time (ms)')
+    axs[0].set_ylabel('Packet Length Sum (Bytes)')
 
     # 绘制第二个子图
     axs[1].bar(range(len(data2)), data2, color = colors[1])
     axs[1].set_title('from 108')
-    axs[1].set_xlabel('Time (0.001s)')
-    axs[1].set_ylabel('Packet Length Sum')
+    axs[1].set_xlabel('Time (ms)')
+    axs[1].set_ylabel('Packet Length Sum (Bytes)')
 
     # 绘制第三个子图
     axs[2].bar(range(len(data3)), data3, color = colors[2])
     axs[2].set_title('from 112')
-    axs[2].set_xlabel('Time (0.001s)')
-    axs[2].set_ylabel('Packet Length Sum')
+    axs[2].set_xlabel('Time (ms)')
+    axs[2].set_ylabel('Packet Length Sum (Bytes)')
 
 
     # 设置整个图的标题
@@ -102,4 +102,13 @@ for epoch_i in range(0, 10):
     plt.tight_layout()
 
     # 展示图像
-    plt.savefig("from0_001s_epoch" + str(epoch_i + 1) + ".png")
+    plt.savefig("./epoch_time/from0_001s/from0_001s_epoch" + str(epoch_i + 1) + ".png")
+
+import multiprocessing
+if __name__ == "__main__":
+    pool = multiprocessing.Pool(processes=10)
+    for epoch_i in range(0, 10):
+        pool.apply_async(draw, (epoch_i, ))
+    pool.close()
+    pool.join()
+    print("Sub-process(es) done.")
