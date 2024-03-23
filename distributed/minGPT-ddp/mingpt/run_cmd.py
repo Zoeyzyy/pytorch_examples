f551f93176f956ea234fac68c1dff571e3e1bef6
Zoeyzyy <1162651439@qq.com>
2024-03-23 20:52:03 +0800

    Update: for nohup shell file

--- distributed/minGPT-ddp/mingpt/run_cmd.py
+++ distributed/minGPT-ddp/mingpt/run_cmd.py
@@ -1,6 +1,7 @@
-local_ip = 112
+local_ip = 106
 
 all_ip = [106, 108, 112]
+# all_ip = [106, 112]
 
 # 打开文件以写入模式
 with open("cmd.sh", "w") as file:
@@ -8,11 +9,11 @@ with open("cmd.sh", "w") as file:
     for ip in all_ip:
         # 向文件中写入内容
         if(ip != local_ip):
-            file.write("sudo tcpdump host 172.22.5." + str(ip) + " -s 128  -w ../../../from" + str(ip) +".pcap & \n")
-            file.write("sudo tcpdump dst 172.22.5." + str(ip) + " -s 128  -w ../../../to" + str(ip) +".pcap & \n")
+            file.write("sudo nohup tcpdump host 172.22.5." + str(ip) + " -s 128  -w ../../../from" + str(ip) +".pcap & \n")
+            file.write("sudo nohup tcpdump dst 172.22.5." + str(ip) + " -s 128  -w ../../../to" + str(ip) +".pcap & \n")
     
     file.write("\n")  
-    file.write("torchrun --master_addr=172.22.5.106 --master_port=22349 --nproc_per_node=1 --nnodes=" + str(len(all_ip)) +" --node_rank="+ str(all_ip.index(local_ip)) +" main.py \n")  
+    file.write("nohup torchrun --master_addr=172.22.5.106 --master_port=22349 --nproc_per_node=1 --nnodes=" + str(len(all_ip)) +" --node_rank="+ str(all_ip.index(local_ip)) +" main.py & \n")  
         
 
 import subprocess
