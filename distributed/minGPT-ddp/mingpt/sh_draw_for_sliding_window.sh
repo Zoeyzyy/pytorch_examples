b1144ac6dd0cf28c413612a45a21d771a36b57c2
Zoeyzyy <1162651439@qq.com>
2024-03-27 22:58:15 +0800

    sh for sliding_window

---
+++ distributed/minGPT-ddp/mingpt/sh_draw_for_sliding_window.sh
@@ -0,0 +1,4 @@
+# nohup python3 draw_for_sliding_window.py --accuracy=12 --from_to=from &
+# nohup python3 draw_for_sliding_window.py --accuracy=12 --from_to=to &
+nohup python3 draw_for_sliding_window.py --accuracy=13 --from_to=from &
+nohup python3 draw_for_sliding_window.py --accuracy=13 --from_to=to &
