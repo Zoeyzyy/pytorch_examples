a11e5a98a61379d0b5a759c2b43c2c2cc60eb422
Zoeyzyy <21110240040@m.fudan.edu.cn>
2024-03-29 20:38:56 +0800

    Fix: figsize is set in subplot

--- distributed/minGPT-ddp/mingpt/draw_for_sliding_window.py
+++ distributed/minGPT-ddp/mingpt/draw_for_sliding_window.py
@@ -39,7 +39,7 @@ for i in range(len(real_epoch_time_plot)):
     sliding_window_epoch_time_plot.append(
         real_epoch_time_plot[0] + epoch_time_gap * i)
 
-from106 = np.loadtxt('./Sum_by_time/Sum_by_Time_from106_'+ str(time_gap)).tolist()
+from112 = np.loadtxt('./Sum_by_time/Sum_by_Time_from112_'+ str(time_gap)).tolist()
 
 def draw_sliding_window(window_i):
     print("draw sliding window ", window_i)
@@ -47,20 +47,19 @@ def draw_sliding_window(window_i):
            sliding_window_epoch_time_plot[0]) * 10 ** (int)(accuracy - 9.0)
     x_r = (int)(sliding_window_epoch_time_plot[window_i + 1] -
            sliding_window_epoch_time_plot[0]) * 10 ** (int)(accuracy - 9.0)
-    print(x_l, x_r, len(from106))
-    data = from106[x_l: x_r]
+    print(x_l, x_r, len(from112))
+    data = from112[x_l: x_r]
     # x 坐标的间隔
     time_interval = 10 ** (int)(- accuracy + 9.0)
     # x 坐标范围
     x_values = [i * time_interval for i in range(len(data))]
 
     # 设置子图布局
-    fig, axs = plt.subplots(1, 1, figsize=(8, 10))
-    plt.figure(figsize=(100, 8))
+    fig, axs = plt.subplots(1, 1, figsize=(100, 8))
     
     # 绘制第一个子图
     axs.bar(x_values, data, color='skyblue', width=float(100.0 / len(x_values)))
-    axs.set_title(str(from_to) + ' 106')
+    axs.set_title(str(from_to) + ' 112')
     axs.set_xlabel('Time (s)')
     axs.set_ylabel('Packet Length Sum (Bytes)')
 
@@ -72,7 +71,7 @@ def draw_sliding_window(window_i):
     plt.tight_layout()
     
     # 展示图像
-    plt.savefig("./window_time/"+ str(from_to)+"_" + str(accuracy) + "/"+str(from_to)+"_106_" +
+    plt.savefig("./window_time/"+ str(from_to)+"_" + str(accuracy) + "/"+str(from_to)+"_112_" +
                 str(accuracy) + "_window" + str(window_i + 1) + ".png")
     plt.close()
 
