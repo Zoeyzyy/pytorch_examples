c1c26807e0181bd04d5e977d0a542012da1fe459
Zoeyzyy <1162651439@qq.com>
2024-03-31 23:30:00 +0800

    Fix: no need for sudo in docker

--- distributed/minGPT-ddp/mingpt100G/run_cmd.py
+++ distributed/minGPT-ddp/mingpt100G/run_cmd.py
@@ -1,15 +1,15 @@
-local_ip = 4
+local_ip = 3
 
 all_ip = [3, 4, 6]
 
 # 打开文件以写入模式
 with open("cmd.sh", "w") as file:
-    file.write("sudo rm ../../../*.pcap \n")
+    file.write("rm ../../../*.pcap \n")
     for ip in all_ip:
         # 向文件中写入内容
         if(ip != local_ip):
-            file.write("sudo nohup tcpdump host 10.2.2." + str(ip) + " -s 128  -w ./packet/from" + str(ip) +".pcap & \n")
-            file.write("sudo nohup tcpdump dst 10.2.2." + str(ip) + " -s 128  -w ./packet/to" + str(ip) +".pcap & \n")
+            file.write("nohup tcpdump host 10.2.2." + str(ip) + " -s 128  -w ./packet/from" + str(ip) +".pcap & \n")
+            file.write("nohup tcpdump dst 10.2.2." + str(ip) + " -s 128  -w ./packet/to" + str(ip) +".pcap & \n")
     
     file.write("\n")  
     file.write("nohup torchrun --master_addr=10.2.2.3 --master_port=22349 --nproc_per_node=1 --nnodes=" + str(len(all_ip)) +" --node_rank="+ str(all_ip.index(local_ip)) +" main.py & \n")  
