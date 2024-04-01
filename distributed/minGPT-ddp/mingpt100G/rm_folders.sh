7e4bf7e61bb8022a0596df840772dd3fb7cf3f60
Zoeyzyy <1162651439@qq.com>
2024-04-01 12:01:51 +0800

    Update: add rm_folders.sh

---
+++ distributed/minGPT-ddp/mingpt100G/rm_folders.sh
@@ -0,0 +1,18 @@
+rm window_time/from_12/*
+rm window_time/from_13/*
+rm window_time/to_12/*
+rm window_time/to_13/*
+
+rm by_time/*
+
+rm by_epoch/*
+
+rm communication_start_distribute/*
+
+rm master/*
+
+rm packet/*
+
+rm step2time_index/*
+
+rm Sum_by_time/*
