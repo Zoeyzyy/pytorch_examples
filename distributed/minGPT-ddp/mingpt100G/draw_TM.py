74118003714886690b157408981b38f535ff3d3b
Zoeyzyy <21110240040@m.fudan.edu.cn>
2024-04-14 23:00:57 +0800

    Update: add draw_TM.py

---
+++ distributed/minGPT-ddp/mingpt100G/draw_TM.py
@@ -0,0 +1,60 @@
+'''
+目的: 画0.01 0.001 0.0001粒度下的在10%处 100 or 1000的流量矩阵图
+方法：
+1. 一个节点汇集所有的Sum_by_Time_node3_from6_0_01或Sum_by_Time_node3_to6_0_01的数据
+2. 依次生成100 or 1000张流量矩阵图
+3. 从10%的index开始, 每张图的数据取自Sum_by_Time_node2_from6的相同index的数据
+
+generate_sum_by_time.py:
+1. 生成Sum_by_Time的数据0.01 0.001 0.0001粒度，以主节点对齐时间
+
+'''
+
+from util import count_lines, get_files_by_suffix, read_th_line
+
+def draw_TM_picture(TM_data, index, time_step):
+    # 画矩阵
+    matrix = np.array(TM_data).reshape(3, 3)
+    
+    # 绘制矩阵图
+    plt.imshow(matrix, cmap='Blues')  # 使用 Blues 颜色映射，数值越大颜色越深
+    plt.colorbar()  # 添加颜色条
+    
+    # 添加坐标轴标签
+    plt.xticks([0, 1, 2], [1, 2, 3])
+    plt.yticks([0, 1, 2], [1, 2, 3])
+    
+    # 添加坐标轴含义
+    plt.xlabel('sender')
+    plt.ylabel('reciever')
+    plt.savefig("TM_index" + str(index) + "_" + str(time_step) + ".png")
+    
+# 计算TM的index，画TM_count个矩阵
+def draw_TMs(time_step, TM_count):
+    start_index = 0.1 * count_lines(f"Sum_by_Time_node3_from6_{time_step}")
+    # 获得./Sum_by_time下的每一个以time_step结尾的文件名
+    names = ["node3_from3", "node3_from4", "node3_from6", "node4_from3", "node4_from4", "node4_from6", "node6_from3", "node6_from4", "node6_from6",]
+    filenames = ["./Sum_by_time/Sum_by_Time_" + name + f"_{time_step}" for name in names]  
+    
+    for i in range(TM_count):
+        index = start_index + i
+        TM_data = []
+        # 遍历filenames，如果文件存在
+        for filename in filenames:
+            if os.path.exists(filename):
+                # 读取第index行的数据
+                line = read_th_line(filename, index)
+                TM_data.append(int(line))
+            else:
+                TM_data.append(0)
+        draw_TM_picture(TM_data, i, time_step)
+                
+        
+
+# 主函数
+if __name__ == '__main__':
+    time_steps = ["0_01", "0_001", "0_0001"]
+    TM_count = [100, 100, 1000]
+    for i in range(len(time_steps)):
+        draw_TMs(time_steps[i], TM_count[i])
+        
---
+++ distributed/minGPT-ddp/mingpt100G/generate_sum_by_time.py
@@ -0,0 +1,56 @@
+'''
+generate_sum_by_time.py:
+目的： 生成Sum_by_Time的数据0.01 0.001 0.0001粒度，以主节点对齐时间
+
+方法：
+1. 主函数：不同粒度, !! 修改node = 4
+
+'''
+
+from util import get_files_by_suffix, read_th_line, count_lines, time_to_float, remove_last_char_if_not_digit, create_file
+
+def generate_sum_by_time(time_step, node):
+    filenames = get_files_by_suffix("./packet", ".txt")
+    
+    master_step_line_number = count_lines("./master/step.txt")
+    start_time = read_th_line("./master/step.txt", 0)
+    start_time = start_time.split()[1]
+    end_time = read_th_line("./master/step.txt", master_step_line_number - 1)
+    end_time = end_time.split()[1]
+    print(start_time, end_time)
+    
+    float_time_step = time_step.replace('_', '.')
+    len_sum_by_time = (int)((time_to_float(end_time) - time_to_float(start_time)) / (float)(float_time_step)) + 1
+    
+    for filename in filenames:
+        sum_by_time = [0] * len_sum_by_time
+        # 打开文件
+        with open("./packet/" + filename, 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                if "seq" in line:
+                    line = line.split()
+                    current_time = time_to_float(line[0])
+                    time_index = (int)((current_time - time_to_float(start_time)) / (float)(float_time_step))
+                    # 寻找"length" 的index
+                    length_index = line.index("length")
+                    if time_index >= 0 and time_index < len_sum_by_time:
+                        sum_by_time[time_index] += remove_last_char_if_not_digit(line[length_index + 1])
+
+            
+            # 存储到文件中
+            from_to = filename.split(".")[0]
+            storename = "./Sum_by_Time/Sum_by_time_node" + str(node) + "_" + from_to + "_" + time_step
+            create_file(storename)
+            # sum_by_time记录到storefile中，每一行一个数据
+            with open(storename, 'w') as f:
+                for i in sum_by_time:
+                    f.write(str(i) + '\n')   
+                
+
+# 主函数
+if __name__ == '__main__':
+    time_steps = ["0_01", "0_001", "0_0001"]
+    node = 6
+    for time_step in time_steps:
+        generate_sum_by_time(time_step, node)
